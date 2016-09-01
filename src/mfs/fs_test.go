6315d4e59b749c42b8a7f4c693221f077fa90fb7
Simon Kirkby <tigger@interthingy.com>
2016-09-01 09:40:42 +0800

    date format for history keeping

--- src/mfs/fs_test.go
+++ src/mfs/fs_test.go
@@ -3,13 +3,21 @@ package mfs
 import (
 	"fmt"
 	"testing"
+	"time"
 )
 
 func TestBasic(t *testing.T) {
 	j := NewIPfsfs()
 	fmt.Println(j)
 	fmt.Println(j.Stat())
-	j.mfs("share")
+	j.Mfs("share")
 	t.Errorf("FAIL")
+
+}
+
+func TestDate(t *testing.T) {
+	const layout = "/2006/01/02/15/04/"
+	n := time.Now()
+	fmt.Println(n.Format(layout))
 
 }
